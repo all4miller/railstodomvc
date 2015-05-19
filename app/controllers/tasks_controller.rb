050744606fe8e98640e66acebf4aab88e06e9895
Angus Miller <all4miller@gmail.com>
2015-05-19 18:17:01 +0200

    updates

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,10 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy, :toggle]
-  before_action :load_open_tasks, except: [:show, :new, :edit]
 
   def index
     @tasks = Task.all
     @task  = Task.new
+    @open_tasks = Task.open_tasks
   end
 
   def show
@@ -20,25 +20,24 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     @task.save
+    @open_tasks = Task.open_tasks
   end
 
   def destroy
     @task.destroy
+    @open_tasks = Task.open_tasks
   end
 
   def toggle
     @task.closed_at = @task.closed? ? nil : Time.now
     @task.save
+    @open_tasks = Task.open_tasks
   end
 
   private
 
   def set_task
     @task = Task.find(params[:id])
-  end
-
-  def load_open_tasks
-    @open_tasks = Task.open_tasks
   end
 
   def task_params
